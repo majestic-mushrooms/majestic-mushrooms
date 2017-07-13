d62118775ebd36a622b9d04f8fb3471b1ec242ab
soo-park <sparkstacks@gmail.com>
2017-07-13 16:29:27 -0700

    semantic boiler plate added

--- client/src/app.js
+++ client/src/app.js
@@ -1,4 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Button } from 'semantic-ui-react'
 
-ReactDOM.render(<h1>Hello World from React</h1>, document.getElementById('root'));
+class App extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+
+        }
+    }
+    render() {
+        return(
+            <div>
+            <Button primary>Primary</Button>
+            <Button secondary>Secondary</Button>
+            </div>
+        )
+    }
+}
+
+ReactDOM.render(<App />, document.getElementById('root'));
