646d1da7eddb4af12c9c9548495515d19d2e5a5e
soo-park <sparkstacks@gmail.com>
2017-07-12 22:39:05 -0700

    passport commented out from config files, config dependancy and require added

--- server/app.js
+++ server/app.js
@@ -3,6 +3,7 @@ const express = require('express');
 const path = require('path');
 const middleware = require('./middleware');
 const routes = require('./routes');
+const config = require('config');
 
 const app = express();
 
