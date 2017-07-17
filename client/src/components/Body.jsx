a3c3480ea0ff8e2b1a6e112409b12d1827bc0f55
Kirk Rohani <kirk.rohani@gmail.com>
2017-07-17 00:25:05 -0700

    Modified mailviewlist to use Redirect

--- client/src/components/Body.jsx
+++ client/src/components/Body.jsx
@@ -1,54 +1,3 @@
-<<<<<<< HEAD
-import React from 'react';
-import SearchBar from './SearchBar.jsx';
-import RightMenu from './RightMenu.jsx';
-import { Sidebar, Segment, Button, Menu, Image, Icon, Header } from 'semantic-ui-react';
-import MailViewList from './MailViewList.jsx';
-
-
-class Body extends React.Component {
-  constructor(props) {
-
-    super(props);
-    this.state = { 
-      visible: true
-    };
-  }
-
-  render() {
-    return (
-      <Sidebar.Pusher>
-
-        <Segment.Group horizontal>
-
-          <Segment basic>
-            <SearchBar />
-            <MailViewList messages={this.props.messages}/>
-          </Segment>
-
-          <Segment basic>
-            <RightMenu />
-          </Segment>
-
-        </Segment.Group>
-
-      </Sidebar.Pusher>
-    );
-  }
-}
-
-export default Body;
-
-
-
-  // <Grid>
-  //   <Grid.Column floated='left' width={5}>
-  //     <Image src='/assets/images/wireframe/paragraph.png' />
-  //   </Grid.Column>
-  //   <Grid.Column floated='right' width={5}>
-  //     <Image src='/assets/images/wireframe/paragraph.png' />
-  //   </Grid.Column>
-=======
 import React from 'react';
 import SearchBar from './SearchBar.jsx';
 import RightMenu from './RightMenu.jsx';
@@ -100,5 +49,4 @@ export default Body;
   //   <Grid.Column floated='right' width={5}>
   //     <Image src='/assets/images/wireframe/paragraph.png' />
   //   </Grid.Column>
->>>>>>> Added more files and started createing nested routes
   // </Grid>
