320c87331e0047e83f3640f3317fa5682c80cf00
Bryant Huang <b.luv.sic@gmail.com>
2017-07-18 10:52:31 -0700

    saved index

--- server/controllers/index.js
+++ server/controllers/index.js
@@ -1,6 +1,3 @@
 module.exports.Profiles = require('./profiles');
-<<<<<<< HEAD
 module.exports.Messages = require('./messages');
-=======
 module.exports.Folders = require('./folders');
->>>>>>> implement dynamic rendering of folderList
