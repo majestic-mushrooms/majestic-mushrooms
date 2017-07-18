f46362fc82de602297870816e93145172ddb4d58
soo-park <sparkstacks@gmail.com>
2017-07-17 23:25:24 -0700

    messages controller and route files added

---
+++ server/routes/messages.js
@@ -0,0 +1,20 @@
+'use strict';
+const express = require('express');
+const router = express.Router();
+const MessageController = require('../controllers').Messages;
+
+router.route('/')
+  .get(MessageController.getAll)
+  // .post(MessageController.create)
+
+router.route('/:id')
+  .get((req, res) => {
+    MessageController.getOne(req, res);
+  })  
+//   .get(MessageController.getOne)  
+//   .put(MessageController.update)
+//   // .delete(MessageController.deleteOne)
+//   ;
+
+
+module.exports = router;
