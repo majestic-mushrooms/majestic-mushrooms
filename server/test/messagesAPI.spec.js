decef812f8fabc8de4fb45faaa3efc1f4b93ec8a
Jennifer Wang <jw775@cornell.edu>
2017-07-22 17:36:46 -0700

    set up basic travis file

---
+++ server/test/messagesAPI.spec.js
@@ -0,0 +1,31 @@
+'use strict';
+const request = require('supertest');
+const express = require('express');
+const expect = require('chai').expect;
+const app = require('../app.js');
+const dbUtils = require('../../db/lib/utils.js');
+
+describe('Messages API', function () {
+  beforeEach(function (done) {
+    dbUtils.rollbackMigrate(done);
+  });
+
+  // Resets database back to original settings
+  afterEach(function (done) {
+    dbUtils.rollback(done);
+  });
+
+  it('accepts GET requests to /api/messages', function (done) {
+    request(app)
+      .get('/api/messages')
+      .expect(res => {
+        res.body = {
+          length: res.body.length
+        };
+      })
+      .expect(200, {
+        length: 1
+      })
+      .end(done);
+  });
+});
