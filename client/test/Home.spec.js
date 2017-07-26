98c64b96a514bce7b301ba888956d7e5afb1e230
soo-park <sparkstacks@gmail.com>
2017-07-25 20:17:45 -0700

    basic tests written for Home & MainRoutes

--- client/test/Home.spec.js
+++ client/test/Home.spec.js
@@ -1,33 +1,25 @@
 import React from 'react'
 import { shallow, mount } from 'enzyme';
 import renderer from 'react-test-renderer'
-// import App from '../src/index.jsx'
 import configureStore from 'redux-mock-store';
 import {Provider} from 'react-redux';
 import {createStore} from 'redux';
+// import App from '../src/index.jsx'
 
 
-describe('>>> <Provider /> --- Shallow Render REACT COMPONENTS', () => {
-    // let wrapper 
-    // beforeEach(()=>{
-    //     wrapper = shallow(<Provider />)
-    // })
+describe('>>> <div /> --- Shallow Render REACT COMPONENTS', () => {
+    let wrapper 
+    beforeEach(()=>{
+        wrapper = shallow(<div />)
+    })
 
-    // it('+++ render the DUMB component', () => {
-    //    console.log("=========================", wrapper);
-
-    //     expect(wrapper.length).toEqual(1);
-    // });
-
-    // it('should render three <BrowserRouter /> components', () => {
-    //     const wrapper = shallow(<Provider />);
-    //     expect(wrapper.length).toBeTruthy();
-    // });
-    
-    it('+++ contains Router - <Provider />', () => {
-    //     expect(wrapper.contains('<Providers />')).toBe(true)
-        var a = 1;
-        var b = 1;
-        expect(a === b);
+    it('+++ render the DUMB component', () => {
+        expect(wrapper.length).toEqual(1);
     });
+
+    it('should render three <div /> components', () => {
+        const wrapper = shallow(<div />);
+        expect(wrapper.length).toBeTruthy();
+    });
+    
 });
