dc62637d7d6fcc06cb769bdd713244f7ec72e38c
soo-park <sparkstacks@gmail.com>
2017-07-25 20:17:45 -0700

    basic tests written for Home & MainRoutes

--- client/test/MainRoutes.spec.js
+++ client/test/MainRoutes.spec.js
@@ -3,17 +3,17 @@ import { shallow } from 'enzyme';
 import { Route } from 'react-router';
 import Routes from '../src/components/Navigation/MainRoutes';
 
-it('renders correct routes', () => {
-  // const wrapper = shallow(<Routes />);
-  // const pathMap = wrapper.find(Route).reduce((pathMap, route) => {
-  //   const routeProps = route.props();
-  //   pathMap[routeProps.path] = routeProps.component;
-  //   return pathMap;
-  // }, {});
-  // // { 'nurse/authorization' : NurseAuthorization, ... }
-
-  // expect(pathMap['/']).toBe('BodyContainer');
-    var a = 1;
-    var b = 1;
-    expect(a === b);
+describe('>>> <Routes /> --- Shallow Render REACT COMPONENTS', () => {
+  
+  it('renders correct routes', () => {
+    const wrapper = shallow(<Routes />);
+    const pathMap = wrapper.find(Route).reduce((pathMap, route) => {
+      const routeProps = route.props();
+      expect(typeof routeProps.render()).toBe('object');
+    //   pathMap[routeProps.path] = routeProps.component;
+    //   return pathMap;
+    }, {});
+    // // { 'nurse/authorization' : NurseAuthorization, ... }
+    // expect(pathMap['/']).toBe('BodyContainer');
+  });
 });
