6a4d9617296349ddc7cd89e2daf59accd4a24d4f
soo-park <sparkstacks@gmail.com>
2017-07-27 19:40:02 -0700

    store testing boiler plate added

--- client/test/store/index.spec.js
+++ client/test/store/index.spec.js
@@ -4,7 +4,7 @@ import renderer from 'react-test-renderer'
 import configureStore from 'redux-mock-store';
 import {Provider} from 'react-redux';
 import {createStore} from 'redux';
-// import App from '../src/index.jsx'
+import {store} from '../../src/store/index.js'
 
 
 describe('>>> <div /> --- Shallow Render REACT COMPONENTS', () => {
