dc131cdc1b7e602fb2e422ee0d0f29aa20c815b6
Jennifer Wang <jw775@cornell.edu>
2017-07-29 11:27:29 -0700

    fix promise nesting

--- server/utils/deltaWatcher.js
+++ server/utils/deltaWatcher.js
@@ -61,11 +61,11 @@ module.exports = function(req) {
           .catch( err => { 
             //try inserting it if it's an error of the message not existing
             console.log('FIRST TRY ERRORED. Trying again.')
-            return models.Message.forge(createMessages([delta.attributes])[0]).save(null, {method: 'insert'});
-          })
-          .then( saved => {
-            console.log('SECOND TRY SUCCESS. Emitting delta.');
-            ee.emit('delta', { event: delta.event, attributes: saved });
+            return models.Message.forge(createMessages([delta.attributes])[0]).save(null, {method: 'insert'})
+            .then( saved => {
+              console.log('SECOND TRY SUCCESS. Emitting delta.');
+              ee.emit('delta', { event: delta.event, attributes: saved });
+            });
           })
           .catch( err => { console.log('ERROR: Message not successfully created/update:', err); });
         }
