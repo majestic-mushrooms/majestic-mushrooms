ac20a3ef72dc66225053a2969118076044ccd453
Jennifer Wang <jw775@cornell.edu>
2017-08-03 10:47:05 -0700

    add dates to parsed messages

--- client/src/components/FolderList.jsx
+++ client/src/components/FolderList.jsx
@@ -28,7 +28,7 @@ class FolderList extends React.Component {
       .then(response => {
         if (response.data.length > 0){
           setAreResults(true);
-          setFilteredMessages(response.data);
+          setFilteredMessages(parseMessages(response.data, today));
         } else {
           console.log('No folder messages found');
           setNewView('Inbox');
