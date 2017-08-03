088be4ffbb1ae7e28d1e18f27104bc187e64fe4d
Jennifer Wang <jw775@cornell.edu>
2017-08-02 19:01:25 -0700

    start sanitizing html

--- db/migrations/emails.js
+++ db/migrations/emails.js
@@ -49,7 +49,7 @@ exports.up = function (knex, Promise) {
       table.foreign('folder_id').references('folders.folder_id');
     }),
     knex.schema.createTableIfNotExists('contacts', function(table) {
-      table.increments('id').unsigned().primary();
+      table.varchar('contact_id').primary();
       table.unique('contact_id');
       table.varchar('email').nullable();
       table.varchar('name').nullable();
